0eea87f0bbd4be1d767e61e84c3ad8ec45e3254b
clean17 <piw940317@gmail.com>
2023-03-22 12:15:22 +0900

    apply Dto 완료

--- src/main/java/shop/mtcoding/project/controller/ApplyController.java
+++ src/main/java/shop/mtcoding/project/controller/ApplyController.java
@@ -1,74 +1,49 @@
 package shop.mtcoding.project.controller;
 
-import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.project.config.annotation.LoginComp;
+import shop.mtcoding.project.config.annotation.LoginUser;
 import shop.mtcoding.project.config.exception.CustomApiException;
 import shop.mtcoding.project.dto.apply.ApplyReq.ApplyReqDto;
 import shop.mtcoding.project.dto.apply.ApplyReq.ApplyUpdateReqDto;
+import shop.mtcoding.project.dto.apply.ApplyResp.ApplyOutDto;
 import shop.mtcoding.project.dto.common.ResponseDto;
 import shop.mtcoding.project.model.comp.Comp;
 import shop.mtcoding.project.model.user.User;
 import shop.mtcoding.project.service.ApplyService;
 
 @Controller
+@RequiredArgsConstructor
 public class ApplyController {
-
-    @Autowired
-    private ApplyService applyService;
-
-    @Autowired
-    private HttpSession session;
+    private final ApplyService applyService;
 
     @PostMapping("/user/apply/resume")
-    public ResponseEntity<?> applyResume(@RequestBody ApplyReqDto aDto) {
-        if (ObjectUtils.isEmpty(aDto.getJobsId())) {
-            throw new CustomApiException("공고 아이디가 없습니다.");
-        }
-        if (ObjectUtils.isEmpty(aDto.getResumeId())) {
-            throw new CustomApiException("이력서 아이디가 없습니다.");
-        }
-        if (ObjectUtils.isEmpty(aDto.getUserId())) {
-            throw new CustomApiException("유저 아이디가 없습니다");
-        }
-        User principal = (User) session.getAttribute("principal");
-        applyService.지원하기(aDto, principal.getUserId());
-
-        return new ResponseEntity<>(new ResponseDto<>(1, "지원 성공", null), HttpStatus.CREATED);
+    public ResponseEntity<?> applyResume(@RequestBody @Valid ApplyReqDto aDto, @LoginUser User user) {
+        ApplyOutDto applyDto = applyService.지원하기(aDto, user.getUserId());
+        return new ResponseEntity<>(new ResponseDto<>(1, "지원 성공", applyDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/comp/apply/update")
-    public ResponseEntity<?> updateApply(@RequestBody ApplyUpdateReqDto aDto) {
-        if (ObjectUtils.isEmpty(aDto.getCompId())) {
-            throw new CustomApiException("기업아이디가 필요합니다.");
-        }
-        if (ObjectUtils.isEmpty(aDto.getApplyId())) {
-            throw new CustomApiException("지원아이디가 필요합니다.");
-        }
-        if (ObjectUtils.isEmpty(aDto.getState())) {
-            throw new CustomApiException("상태정보가 필요합니다.");
-        }
+    public ResponseEntity<?> updateApply(@RequestBody @Valid ApplyUpdateReqDto aDto, @LoginComp Comp comp) {
         if (!(aDto.getState() == 1 || aDto.getState() == -1)) {
             throw new CustomApiException("상태정보가 다릅니다.");
         }
-        Comp compSession = (Comp) session.getAttribute("compSession");
         Integer result = 0;
         if (aDto.getState() == 1) {
-            applyService.합격(aDto, compSession.getCompId());
-            result = 1;
+            ApplyOutDto applyDto = applyService.합격(aDto, comp.getCompId());
+            return new ResponseEntity<>(new ResponseDto<>(1, "지원 결과는 합격입니다.", applyDto), HttpStatus.OK);
+        }else {
+            ApplyOutDto applyDto = applyService.불합격(aDto, comp.getCompId());
+            return new ResponseEntity<>(new ResponseDto<>(1, "지원 결과는 불합격입니다.", applyDto), HttpStatus.OK);
         }
-        if (aDto.getState() == -1) {
-            applyService.불합격(aDto, compSession.getCompId());
-            result = -1;
-        }
-        return new ResponseEntity<>(new ResponseDto<>(1, "지원자에게 답변했습니다.", result), HttpStatus.OK);
     }
 }
--- src/main/java/shop/mtcoding/project/dto/apply/ApplyReq.java
+++ src/main/java/shop/mtcoding/project/dto/apply/ApplyReq.java
@@ -1,24 +1,41 @@
 package shop.mtcoding.project.dto.apply;
 
+
+
+import java.sql.Timestamp;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 public class ApplyReq {
     
     @Getter
     @Setter
     public static class ApplyReqDto{
+        @NotNull(message = "이력서 아이디가 없습니다/")
         private Integer resumeId;
+        @NotNull(message = "공고 아이디가 없습니다/")
         private Integer jobsId;
+        @NotNull(message = "유저 아이디가 없습니다/")
         private Integer userId;
+        @NotNull(message = "이력서 아이디가 없습니다/")
         private Integer applyId;
     }
+
+    
 
     @Getter
     @Setter
     public static class ApplyUpdateReqDto{
+        @NotNull(message = "지원아이디가 필요합니다/")
         private Integer applyId;
+        @NotNull(message = "기업아이디가 필요합니다/")
         private Integer compId;
+        @NotNull(message = "상태정보가 필요합니다/")
         private Integer state;
     }
     
